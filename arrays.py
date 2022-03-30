85c6bd3233ccf55920c0b0facaed8dce273868e9
emnaruto07 <shazebvps@gmail.com>
2022-03-30 22:29:13 +0530

    x

--- arrays.py
+++ arrays.py
@@ -17,25 +17,37 @@
 
 # reverseInterger
 
-def reverse(arr, n):
-    start = 0
-    end = n-1
+# def reverse(arr, n):
+#     start = 0
+#     end = n-1
 
-    while start<=end:
-        arr[start],arr[end] = arr[end],arr[start]
-        start += 1
-        end -= 1
+#     while start<=end:
+#         arr[start],arr[end] = arr[end],arr[start]
+#         start += 1
+#         end -= 1
+def swap(arr):
+    for i in range(arr):
+        if (i+1 < len(arr)):
+            temp = arr[1]
+            arr[1] = arr[0]
+            arr[0] = temp
+
+
+def swapAlternative(arr, n):
+    for i in range(n):
+        if (i+1 < n):
+            swap(arr)
 
 def printArray(arr, n):
-    for i in range(len(1<n)):
+    for i in range(n):
         print(arr[i])
 
 
 arr = [1,3,7,4,6,7]
-brr = [4,5,7,8,5]
+# brr = [4,5,7,8,5]
 
-reverse(arr, 6)
-reverse(arr, 5)
+swapAlternative(arr, 6)
+# reverse(arr, 5)
 
 printArray(arr, 6)
-printArray(brr, 5)
+# printArray(brr, 5)
